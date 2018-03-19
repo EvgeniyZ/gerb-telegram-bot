62b490e1d72fee516954ce10e5cf560062572b47
EvgeniyZ <rocketjump07@gmail.com>
2018-03-19 22:34:22 +0300

    add initial sections.

--- src/Infrastructure/ContextInitializer.cs
+++ src/Infrastructure/ContextInitializer.cs
@@ -17,7 +17,7 @@ namespace Gerb.Telegram.Bot.Infrastructure
             {
                 new Section
                 {
-                    Name = "ХЛЕБ И МУЧНЫЕ ИЗДЕЛИЯ",
+                    Name = "Хлеб и мучные изделия",
                     AllowedDescription = "Хлеб пшеничный вчерашней выпечки или подсушенный; сухой бисквит, печенье сухое. 1-2 раза в неделю хорошо выпеченные несдобные булочки, печеные пирожки с яблоками, отварным мясом или рыбой и яйцами, джемом, ватрушка с творогом.",
                     ForbiddenDescription = "Ржаной и любой свежий хлеб, изделия из сдобного и слоёного теста.",
                     Recommendations = new List<Recommendation>
@@ -64,7 +64,7 @@ namespace Gerb.Telegram.Bot.Infrastructure
                 },
                 new Section
                 {
-                    Name = "СУПЫ",
+                    Name = "Супы",
                     AllowedDescription = "Из разрешённых протёртых овощей на морковном, картофельном отваре, молочные супы из протёртых или хорошо разваренных круп (геркулес, манная, рис и др.), вермишели с добавлением протёртых овощей, молочные супы-пюре из протёртых овощей; суп-пюре из заранее вываренных кур и мяса. Муку для супов только подсушивают. Супы заправляют сливочным маслом, яично-молочной смесью, сливками.",
                     ForbiddenDescription = "Мясные и рыбные бульоны, грибные и крепкие овощные отвары, щи, борщи, окрошку.",
                     Recommendations = new List<Recommendation>
@@ -118,7 +118,7 @@ namespace Gerb.Telegram.Bot.Infrastructure
                 },
                 new Section
                 {
-                    Name = "МЯСО И ПТИЦА",
+                    Name = "Мясо и птица",
                     AllowedDescription = "Паровые и отварные блюда из языка, печени, нежирной телятины, говядины, молодой нежирной баранины и обрезной свинины, кур, индейки, кролика. Паровые котлеты, биточки, кнели, суфле, пюре, зразы; бефстроганов из варёного мяса.",
                     ForbiddenDescription = "Жирные или жилистые сорта мяса и птиц, утку, гуся, консервы, копчёности.",
                     Recommendations = new List<Recommendation>
@@ -172,7 +172,7 @@ namespace Gerb.Telegram.Bot.Infrastructure
                 },
                 new Section
                 {
-                    Name = "РЫБА",
+                    Name = "Рыба",
                     AllowedDescription = "Нежирные виды без кожи, куском или в виде котлетной массы; варится в воде или на пару.",
                     ForbiddenDescription = "Жирную, солёную рыбу, консервы.",
                     Recommendations = new List<Recommendation>(),
@@ -180,7 +180,7 @@ namespace Gerb.Telegram.Bot.Infrastructure
                 },
                 new Section
                 {
-                    Name = "МОЛОЧНЫЕ ПРОДУКТЫ",
+                    Name = "Молочные продукты",
                     AllowedDescription = "Молоко, сливки, некислый кефир, простокваша, ацидофилин, свежий некислый творог (протёртый). Творожные блюда: запечённые сырники, суфле, ленивые вареники, пудинги, неострый сыр.",
                     ForbiddenDescription = "Молочные продукты с высокой кислотностью, острые, солёные сыры. Ограничивают сметану.",
                     Recommendations = new List<Recommendation>
@@ -241,7 +241,7 @@ namespace Gerb.Telegram.Bot.Infrastructure
                 },
                 new Section
                 {
-                    Name = "ЯЙЦА",
+                    Name = "Яйца",
                     AllowedDescription = "2-3 штуки в день. Всмятку, паровой омлет.",
                     ForbiddenDescription = "Яйца вкрутую, жареные.",
                     Recommendations = new List<Recommendation>
@@ -258,7 +258,7 @@ namespace Gerb.Telegram.Bot.Infrastructure
                 },
                 new Section
                 {
-                    Name = "КРУПЫ",
+                    Name = "Крупы",
                     AllowedDescription = "Манная, гречневая, освяная, рис. Каши, сваренные на молоке или воде, полувязкие и протёртые. Вермишель, макароны отварные.",
                     ForbiddenDescription = "Пшено, перловую, ячневую, кукурузную крупу, бобовые.",
                     Recommendations = new List<Recommendation>
@@ -319,15 +319,150 @@ namespace Gerb.Telegram.Bot.Infrastructure
                 },
                 new Section
                 {
-                    Name = "ОВОЩИ",
+                    Name = "Овощи",
                     AllowedDescription = "Картофель, морковь, свекла, цветная капуста, ограничено - зелёный горошек. Сваренные на пару или в воде и протёртые (пюре, суфле, паровые пудинги). Непротёртые ранние тыква и кабачки. Мелкошинкованный укроп - в супы. Спелые некислые томаты до 100г.",
                     ForbiddenDescription = "Белокочанную капусту, репу, брюкву, редьку, щавель, шпинат, лук, огурцы, солёные, квашенные  маринованные овощи, грибы, овощные закусочные консервы.",
-                    Recommendations = new List<Recommendation>(),
-                    Restrictions = new List<Restriction>()
+                    Recommendations = new List<Recommendation>
+                    {
+                        new Recommendation
+                        {
+                            Food = new Food
+                            {
+                                Name = "Картофель"
+                            }
+                        },
+                        new Recommendation
+                        {
+                            Food = new Food
+                            {
+                                Name = "Морковь"
+                            }
+                        },
+                        new Recommendation
+                        {
+                            Food = new Food
+                            {
+                                Name = "Свекла"
+                            }
+                        },
+                        new Recommendation
+                        {
+                            Food = new Food
+                            {
+                                Name = "Кабачок"
+                            }
+                        },
+                    },
+                    Restrictions = new List<Restriction>
+                    {
+                        new Restriction
+                        {
+                            Food = new Food
+                            {
+                                Name = "Репа"
+                            }
+                        },
+                        new Restriction
+                        {
+                            Food = new Food
+                            {
+                                Name = "Брюква"
+                            }
+                        },
+                        new Restriction
+                        {
+                            Food = new Food
+                            {
+                                Name = "Редька"
+                            }
+                        },
+                        new Restriction
+                        {
+                            Food = new Food
+                            {
+                                Name = "Щавель"
+                            }
+                        },
+                        new Restriction
+                        {
+                            Food = new Food
+                            {
+                                Name = "Шпинат"
+                            }
+                        },
+                        new Restriction
+                        {
+                            Food = new Food
+                            {
+                                Name = "Лук"
+                            }
+                        },
+                        new Restriction
+                        {
+                            Food = new Food
+                            {
+                                Name = "Огурцы"
+                            }
+                        },
+                    }
                 },
                 new Section
                 {
-                    Name = "ПЛОДЫ, СЛАДКИЕ БЛЮДА, СЛАДОСТИ",
+                    Name = "Закуски",
+                    AllowedDescription = "Салаты из отварных овощей, мяса, рыбы. Язык отварной; паштет из печени; колбасы диетические; заливная рыба на овощном отваре; изредка вымоченная нежирная сельдь и форшмак; неострый сыр, несолёная ветчина без жира.",
+                    ForbiddenDescription = "все острые и солёные закуски, консервы, копчёности",
+                    Recommendations = new List<Recommendation>
+                    {
+                        new Recommendation
+                        {
+                            Food = new Food
+                            {
+                                Name = "Язык"
+                            }
+                        },
+                        new Recommendation
+                        {
+                            Food = new Food
+                            {
+                                Name = "Паштет"
+                            }
+                        },
+                    },
+                    Restrictions = new List<Restriction>
+                    {
+                        new Restriction
+                        {
+                            Food = new Food
+                            {
+                                Name = "Пицца"
+                            }
+                        },
+                        new Restriction
+                        {
+                            Food = new Food
+                            {
+                                Name = "Чипсы"
+                            }
+                        },
+                        new Restriction
+                        {
+                            Food = new Food
+                            {
+                                Name = "Наггетцы"
+                            }
+                        },
+                        new Restriction
+                        {
+                            Food = new Food
+                            {
+                                Name = "Фастфуд"
+                            }
+                        },
+                    }
+                },
+                new Section
+                {
+                    Name = "Плоды, сладкие блюда, сладости",
                     AllowedDescription = "В протёртом, варёном и печёном виде сладкие ягоды и фрукты. Пюре, кисели, муссы, желе, компоты, сливочный крем, молочный кисель. Сахар, мёд, некислое варенье, зефир, пастила.",
                     ForbiddenDescription = "Кислые, недостаточно спелые, богатые клетчаткой фрукты и ягоды, непротёртые сухофрукты, шоколад, мороженое.",
                     Recommendations = new List<Recommendation>
@@ -364,6 +499,99 @@ namespace Gerb.Telegram.Bot.Infrastructure
                             }
                         },
                     },
+                },
+                new Section
+                {
+                    Name = "Соусы и пряности",
+                    AllowedDescription = "Молочный (бешамель) без пассировки муки, с добавлением сливочного масла, сметаны, фруктовые, молочно-фруктовые. Ограничено - укроп, петрушка, ванилин, корица.",
+                    ForbiddenDescription = "Томатные соусы, хрен, горчица, перец.",
+                    Recommendations = new List<Recommendation>(),
+                    Restrictions = new List<Restriction> 
+                    {
+                        new Restriction 
+                        {
+                            Food = new Food 
+                            {
+                                Name = "Перец"
+                            }
+                        },
+                        new Restriction 
+                        {
+                            Food = new Food 
+                            {
+                                Name = "Хрен"
+                            }
+                        },
+                        new Restriction 
+                        {
+                            Food = new Food 
+                            {
+                                Name = "Горчица"
+                            }
+                        },
+                        new Restriction 
+                        {
+                            Food = new Food 
+                            {
+                                Name = "Томаты"
+                            }
+                        },
+                        new Restriction 
+                        {
+                            Food = new Food 
+                            {
+                                Name = "Помидоры"
+                            }
+                        },
+                    }
+                },
+                new Section
+                {
+                    Name = "Напитки",
+                    AllowedDescription = "Некрепкий чай, чай с молоком, сливками, слабые какое и кофе с молоком. Сладкие соки из фруктов и ягод. Отвар шиповника.",
+                    ForbiddenDescription = "Алкогольные и газированные напитки, квас, чёрный кофе.",
+                    Recommendations = new List<Recommendation> 
+                    {
+                        new Recommendation 
+                        {
+                            Food = new Food 
+                            {
+                                Name = "Молоко"
+                            }
+                        }
+                    },
+                    Restrictions = new List<Restriction> 
+                    {
+                        new Restriction 
+                        {
+                            Food = new Food 
+                            {
+                                Name = "Квас"
+                            }
+                        },
+                        new Restriction 
+                        {
+                            Food = new Food 
+                            {
+                                Name = "Кола"
+                            }
+                        },
+                        new Restriction 
+                        {
+                            Food = new Food 
+                            {
+                                Name = "Кофе"
+                            }
+                        },
+                    }
+                },
+                new Section
+                {
+                    Name = "Жиры",
+                    AllowedDescription = "Сливочное несолёное масло, коровье топлёное высшего сорта. Рафинированные растительные масла, добавляемые в блюда.",
+                    ForbiddenDescription = "Все другие жиры.",
+                    Recommendations = new List<Recommendation>(),
+                    Restrictions = new List<Restriction>()
                 }
             };
             foreach (var section in sections)
